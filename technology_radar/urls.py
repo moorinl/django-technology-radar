bd1318c8881fc0ae7f0ea245324309410c700550
Rob Moorman <r.moorman@hw843-rmoorman.local>
2016-02-11 23:02:54 +0100

    simplified tox flake8 args added radar detail rest api

--- technology_radar/urls.py
+++ technology_radar/urls.py
@@ -6,4 +6,6 @@ from technology_radar import views
 urlpatterns = [
     url(r'^$', views.ApiRootView.as_view(), name='api-root'),
     url(r'^radars/$', views.RadarListView.as_view(), name='radar-list'),
+    url(r'^radars/(?P<pk>[0-9]+)/$', views.RadarDetailView.as_view(),
+        name='radar-detail'),
 ]
--- technology_radar/views/radar.py
+++ technology_radar/views/radar.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,11 +8,18 @@ from technology_radar.models import Radar
 from technology_radar.serializers import RadarSerializer
 
 
-__all__ = ['RadarListView']
+__all__ = ['RadarListView', 'RadarDetailView']
 
 
 class RadarListView(APIView):
     def get(self, request, format=None):
         qs = Radar.objects.all()
         ser = RadarSerializer(qs, many=True)
+        return Response(ser.data)
+
+
+class RadarDetailView(APIView):
+    def get(self, request, pk, format=None):
+        obj = get_object_or_404(Radar, pk=pk)
+        ser = RadarSerializer(obj)
         return Response(ser.data)
