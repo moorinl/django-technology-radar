b4dc8b5bdb5a61a73aa1299e72f06aa23e85b0ed
Rob Moorman <r.moorman@hw843-rmoorman.local>
2016-02-15 18:10:47 +0100

    added initial pdf renderer

--- technology_radar/renderers.py
+++ technology_radar/renderers.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
+from reportlab.pdfgen import canvas
+
 
 class AbstractRenderer(object):
     def render(self, request, radar):
@@ -11,7 +13,17 @@ class JPGRenderer(AbstractRenderer):
 
 
 class PDFRenderer(AbstractRenderer):
-    pass
+    def render(self, request, radar):
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="%s.pdf"' % (
+                                          radar.slug)
+
+        pdf = canvas.Canvas(response)
+        pdf.drawString(100, 100, radar.name)
+        pdf.showPage()
+        pdf.save()
+
+        return response
 
 
 class PNGRenderer(AbstractRenderer):
